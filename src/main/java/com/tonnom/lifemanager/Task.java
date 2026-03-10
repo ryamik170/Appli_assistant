c4a21849e6b869f3c22140c7e471e1221b77afb0
unknown <68517@etu.he2b.be>
2026-03-10 17:29:10 +0100

    Probleme avec Maven + Javafx tjr pas résolu, ajout des fonctions necessaire à la Todo-list

---
+++ src/main/java/com/tonnom/lifemanager/Task.java
@@ -0,0 +1,18 @@
+package com.tonnom.lifemanager;
+
+public class Task {
+    private String text;
+
+    public Task(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
---
+++ src/main/java/com/tonnom/lifemanager/controllers/TodolistController.java
@@ -0,0 +1,11 @@
+package com.tonnom.lifemanager.controllers;
+
+import com.tonnom.lifemanager.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
