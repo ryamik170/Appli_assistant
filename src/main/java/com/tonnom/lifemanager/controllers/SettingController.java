38514ff0edda73409575f430a24a5e92bfca6f5b
unknown <68517@etu.he2b.be>
2026-04-12 00:18:15 +0200

    ajout et modification de fichiers

--- src/main/java/com/tonnom/lifemanager/controllers/SettingController.java
+++ src/main/java/com/tonnom/lifemanager/controllers/SettingController.java
@@ -1,8 +1,40 @@
 package com.tonnom.lifemanager.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import java.io.IOException;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+
 //Pour le moment on gerera que 2 thèmes pour l'appli : clair et sombre
 
 public class SettingController {
 
-    public void theme_zone() {} //fct qui me permettra d'ouvrir une nouvelle page donnant la possibilité a l'user de choisir un theme
+    @FXML
+    private Button btn;
+
+    @FXML
+    private VBox root;
+
+    private void New_page(String page) {
+        try {
+            Parent new_page = FXMLLoader.load(getClass().getResource("/view/" + page));
+
+            root.getChildren().setAll(new_page); //on remplace le contenu de setting
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void elems_in_setting() {
+        String text = btn.getText();
+        System.out.println(text);
+
+
+        New_page(text + "-view.fxml");
+
+    }   
 }
---
+++ src/main/java/com/tonnom/lifemanager/controllers/TakeDataController.java
@@ -0,0 +1,19 @@
+package com.tonnom.lifemanager.controllers;
+
+public class TakeDataController {
+
+    //fonction qui va parser le fichier
+
+
+    //fonction qui va gérer les jours
+
+
+    //fonction qui va gérer les mois
+
+
+    //fonction qui va gérer les années
+
+
+    //fonction qui va gérer les graphes
+}
+
